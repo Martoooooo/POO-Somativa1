7dc5da5579a17dad22910afa8301951361b9a445
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2021-09-14 17:23:05 -0300

    Atribuição de métodos e atributos

--- Banco/src/Banco/Transacao.java
+++ Banco/src/Banco/Transacao.java
@@ -1,4 +1,48 @@
 package Banco;
 
+
 public class Transacao {
+    private String descricao;
+    private int data;
+    private Float valor;
+
+
+    public Transacao(String descricao, int data, Float valor) {
+        this.descricao = descricao;
+        this.data = data;
+        this.valor = valor;
+    }
+
+
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Float getValor() {
+        return this.valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public void imprimir() {
+        System.out.println("Data da transação: "+ getData());
+        System.out.println("Descrição: "+ getDescricao());
+        System.out.println("Valor: R$ "+ getValor());        
+        
+    }
 }
